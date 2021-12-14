aae956126f77787bef148708283699ab5b6db772
Paige Tiedeman <paigetiedeman@gmail.com>
2021-12-13 23:11:21 -0800

    Updates

---
+++ precache-manifest.41eb2dc80dcb77e9941f5112a2b0738d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "780c5521a388d16e3ba5f4e3f62b5444",
+    "url": "/elevate-everyday/index.html"
+  },
+  {
+    "revision": "32911cb508ba27c4ae6c",
+    "url": "/elevate-everyday/static/css/2.68a81a89.chunk.css"
+  },
+  {
+    "revision": "ae1b9f4283edd67aeb22",
+    "url": "/elevate-everyday/static/css/main.1fa3f329.chunk.css"
+  },
+  {
+    "revision": "32911cb508ba27c4ae6c",
+    "url": "/elevate-everyday/static/js/2.0a6cb97c.chunk.js"
+  },
+  {
+    "revision": "109b70bd7d736f6ebe25",
+    "url": "/elevate-everyday/static/js/3.fdbf0c29.chunk.js"
+  },
+  {
+    "revision": "ae1b9f4283edd67aeb22",
+    "url": "/elevate-everyday/static/js/main.bb0f3889.chunk.js"
+  },
+  {
+    "revision": "c008bfabe5803f924616",
+    "url": "/elevate-everyday/static/js/runtime-main.0310bf11.js"
+  }
+]);
