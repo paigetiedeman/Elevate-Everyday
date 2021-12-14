0661fdbd44b9cc50557da779e73081bc645d7d6f
Paige Tiedeman <paigetiedeman@gmail.com>
2021-12-13 22:18:14 -0800

    WIP: Signup and Authentication

--- src/Components/App.js
+++ src/Components/App.js
@@ -4,8 +4,10 @@ import WorkoutControl from './Workout/WorkoutControl'
 import Footer from './Footer/Footer'
 import Calendar from './Calendar/Calendar'
 import Blog from './Blog/Blog'
+import SignUp from './Authentication/SignUp'
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { AuthProvider } from '../contexts/AuthContext';
 import { db } from '../firebase'
 import { collection, getDocs } from 'firebase/firestore'
 
@@ -13,38 +15,41 @@ import { collection, getDocs } from 'firebase/firestore'
 
 function App() {
 
-  const [users, setUsers] = useState([]);
-  const usersCollectionRef = collection(db, "users");
+  // const [users, setUsers] = useState([]);
+  // const usersCollectionRef = collection(db, "users");
 
-  useEffect(() => {
-    const getUsers = async () => {
-      const data = await getDocs(usersCollectionRef);
-      setUsers(data.docs.map((doc) => ({...doc.data(), id: doc.id })));
-    };
-    getUsers();
-  }, []);
+  // useEffect(() => {
+  //   const getUsers = async () => {
+  //     const data = await getDocs(usersCollectionRef);
+  //     setUsers(data.docs.map((doc) => ({...doc.data(), id: doc.id })));
+  //   };
+  //   getUsers();
+  // }, []);
+  {/* {users.map((user) => {
+return <div><h1>Welcome {user.email}!</h1></div>;
+})} */}
 
   return (
-    <Router>
-      <Header />
-    <div className="container"> 
-      <Switch>
-        <Route exact path="/">
-          <WorkoutControl />
-          {users.map((user) => {
-      return <div><h1>Welcome {user.email}!</h1></div>;
-        })}
-        </Route>
-        <Route path="/calendar">
-          <Calendar />
-        </Route>
-        <Route path="/blog">
-          <Blog />
-        </Route>
-      </Switch>
-      <Footer />
-    </div>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Header />
+      <div className="container"> 
+        <Switch>
+          <Route exact path="/">
+            <WorkoutControl />
+            <SignUp />
+          </Route>
+          <Route path="/calendar">
+            <Calendar />
+          </Route>
+          <Route path="/blog">
+            <Blog />
+          </Route>
+        </Switch>
+        <Footer />
+      </div>
+      </Router>
+    </AuthProvider>
   );
 }
 
--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,7 @@
-import { getFirestore } from "firebase/firestore";
-import { initializeApp } from 'firebase/app';
+import firebase from 'firebase/compat/app';
+import "firebase/compat/auth"
 
-
-const firebaseConfig = {
+const app = firebase.initializeApp({
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
@@ -10,8 +9,8 @@ const firebaseConfig = {
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID 
-}
+})
 
-const app = initializeApp(firebaseConfig);
+export const auth = app.auth();
 
-export const db = getFirestore(app);
+export default app;
