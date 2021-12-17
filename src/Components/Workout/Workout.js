94171afaf573d1ffd58600e389bc945b4e8f7614
Paige Tiedeman <paigetiedeman@gmail.com>
2021-12-17 09:20:44 -0800

    Semantic UI and workout started

--- src/Components/Workout/Workout.js
+++ src/Components/Workout/Workout.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Card, Image, Button } from 'semantic-ui-react';
 
-export default function Workout() {
+export default function Workout(props) {
   return (
-    <div>
-      
-    </div>
-  )
+    <Card.Group>
+      <Card>
+        <Card.Content>
+          <Image floated="left" size="mini" src={props.img} />
+          <Card.Header>{props.title}</Card.Header>
+          <Card.Meta>{props.duration} minutes</Card.Meta>
+          <Card.Description>{props.equipment}</Card.Description>
+          <Button color='teal'>
+            Details
+          </Button>
+        </Card.Content>
+        <Card.Content>{props.tags}</Card.Content>
+      </Card>
+    </Card.Group>
+  );
+}
+
+Workout.propTypes = {
+  img: PropTypes.string,
+  title: PropTypes.string,
+  duration: PropTypes.number,
+  equipment: PropTypes.string,
+  tags: PropTypes.string,
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import App from './Components/App';
 import reportWebVitals from './reportWebVitals';
+import 'semantic-ui-css/semantic.min.css';
 
 
 ReactDOM.render(
