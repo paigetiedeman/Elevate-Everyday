a64a9f3e8244f39fcfd587ba053327ad9074ad01
Paige Tiedeman <paigetiedeman@gmail.com>
2021-12-17 13:11:22 -0800

    WIP: control/redux

--- src/Components/Workout/WorkoutControl.js
+++ src/Components/Workout/WorkoutControl.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from 'react';
+import NewWorkoutForm from './NewWorkoutForm';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { withFirestore, isLoaded } from 'react-redux-firebase';
 
-export default function WorkoutControl() {
+
+class WorkoutControl extends React.Component {
+
+    constructor(props) {
+      super(props);
+      this.state = {
+        selectedWorkout: null,
+        editing: false
+      }
+    }
+
+    handleAddingWorkout = () => {
+      
+    }
+
+    
+  render() {
   return (
     <div>
       <h1>Workout Control</h1>
     </div>
   )
+  }
 }
+
+export default WorkoutControl;
---
+++ src/actions/index.js
@@ -0,0 +1,10 @@
+import * as c from './ActionTypes';
+
+export const seeWorkouts = () => ({
+  type: c.SEE_WORKOUTS
+});
+
+export const deleteWorkout = () => ({
+  type: c.DELETE_WORKOUT
+});
+
