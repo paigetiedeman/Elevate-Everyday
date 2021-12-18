b553bcfd6bf80aed0b81c6f377c1adc4446ccdef
Paige Tiedeman <paigetiedeman@gmail.com>
2021-12-17 17:12:08 -0800

    WIP: WorkoutControl (power going out)

--- src/Components/Workout/WorkoutControl.js
+++ src/Components/Workout/WorkoutControl.js
@@ -1,32 +1,95 @@
 import React from 'react';
 import NewWorkoutForm from './NewWorkoutForm';
+import EditWorkout from './EditWorkout';
+import WorkoutDetail from './WorkoutDetail';
+import WorkoutList from './WorkoutList';
+import * as a from './../../actions';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { withFirestore, isLoaded } from 'react-redux-firebase';
-
+import { Button } from 'semantic-ui-react';
 
 class WorkoutControl extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedWorkout: null,
+      editing: false,
+    };
+  }
 
-    constructor(props) {
-      super(props);
-      this.state = {
+  handleClick = () => {
+    if (this.state.selectedWorkout != null) {
+      this.setState({
         selectedWorkout: null,
-        editing: false
+        editing: false,
+      });
+    } else {
+      const { dispatch } = this.props;
+      const action = a.seeWorkouts();
+      dispatch(action);
+    }
+  };
+
+  handleAddingWorkout = () => {
+    const { dispatch } = this.props;
+    const action = a.seeWorkouts();
+    dispatch(action);
+  };
+
+  handleChangingWorkout = (id) => {
+    this.props.firestore.get({collection: 'workouts', doc: id}).then((workout) => {
+      const firestoreWorkout = {
+        
       }
-    }
+    })
+  }
 
-    handleAddingWorkout = () => {
-      
-    }
-
-    
   render() {
-  return (
-    <div>
-      <h1>Workout Control</h1>
-    </div>
-  )
+    let currentlyVisibleState = null;
+    let buttonText = null;
+    if (this.state.editing) {
+      currentlyVisibleState = (
+        <EditWorkout workout={this.state.selectedWorkout} />
+      );
+      buttonText = 'Return to Workouts';
+    } else if (this.state.selectedWorkout != null) {
+      currentlyVisibleState = (
+        <WorkoutDetail workout={this.state.selectedWorkout} />
+      );
+      buttonText = 'Return to Workouts';
+    } else if (this.props.formVisibleOnPage) {
+      currentlyVisibleState = <NewWorkoutForm />;
+      buttonText = 'Return to Workouts';
+    } else {
+      currentlyVisibleState = (
+        <WorkoutList workoutList={this.props.mainWorkoutList} />
+      );
+      buttonText = 'Add a New Workout';
+    }
+    return (
+      <>
+        {currentlyVisibleState}
+        <Button color="teal" onClick={this.handleClick}>
+          {buttonText}
+        </Button>
+      </>
+    );
   }
 }
 
-export default WorkoutControl;
+WorkoutControl.propTypes = {
+  mainWorkoutList: PropTypes.object,
+  formVisibleOnPage: PropTypes.bool,
+};
+
+const mapStateToProps = (state) => {
+  return {
+    mainWorkoutList: state.mainWorkoutList,
+    formVisibleOnPage: state.formVisibleOnPage,
+  };
+};
+
+WorkoutControl = connect(mapStateToProps)(WorkoutControl);
+
+export default withFirestore(WorkoutControl);
--- src/reducers/workout-list-reducer.js
+++ src/reducers/workout-list-reducer.js
@@ -1,6 +1,7 @@
 import * as c from './../actions/ActionTypes';
 
 export default (state = {}, action ) => {
+  const { id } = action;
   switch (action.type) {
     case c.DELETE_WORKOUT: 
       const newState = {...state};
