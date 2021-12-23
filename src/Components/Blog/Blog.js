c8674cbd58c05c9785170712f25fbdb9e1fb73bc
Paige Tiedeman <paigetiedeman@gmail.com>
2021-12-23 09:58:10 -0800

    Adding Blog components

--- src/Components/Blog/Blog.js
+++ src/Components/Blog/Blog.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Card, Image, Button, Icon } from 'semantic-ui-react';
 
 export default function Blog() {
   return (
-    <div className="Blog">
-      <h1>Blog</h1>
-    </div>
-  )
+    <Card color="pink">
+      <Card.Content>
+        <Image floated="right" src={props.photo} />
+        <Card.Header>{props.title}</Card.Header>
+        <Card.Meta>By: {props.author}</Card.Meta>
+        <Card.Description>{props.snippet}</Card.Description>
+        <Button color="purple">See More</Button>
+      </Card.Content>
+      <Card.Content>
+        <Icon name="calendar alternate outline" /> Posted: {props.date}
+      </Card.Content>
+    </Card>
+  );
 }
+
+Blog.propTypes = {
+  title: PropTypes.string,
+  author: PropTypes.string,
+  photo: PropTypes.any,
+  snippet: PropTypes.string,
+  date: PropTypes.date,
+  content: PropTypes.string,
+};
---
+++ src/Components/Blog/BlogList.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+const blogList = [
+  { 
+    title: "Trainer Tip Tuesday: Winter Motivation",
+    author: "By: Emily Freeman",
+    photo: "https://images.unsplash.com/photo-1606902965551-dce093cda6e7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+    snippet: "These 6 tips will help you embrace this season and stay motivated during these colder months.",
+    date: "December, 12, 2021",
+    content: "#1 Take your workouts outdoors, #2 Make it social, #3 Bundle Up before you go outside, #4 Press play on your favorite at home workout video, #5 Take advantage of your lunch hour, #6 Exercise both mind and Body.",
+  },
+  { 
+    title: "Easy Resolutions to Start Your Fitness Journey",
+    author: "By: Emilia Benton",
+    photo: "https://images.unsplash.com/photo-1577046848400-481714c13218?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+    snippet: "Here are eight attainable fitness resolutions you can easily make time for, no matter what your lifestyle is currently like.",
+    date: "December, 20, 2021",
+    content: "#1 Train for a 5k, #2 Hold a plank, #3 Drink more water, #4 Walk more, #5 Eat more fruit, #6 Try a new fitness class, #7 Get more sleep, #8 Work out in the morning.",
+  },
+  { 
+    title: "Top Superfoods you Should be Eating",
+    author: "By: Natalie Olsen",
+    photo: "https://images.unsplash.com/photo-1490645935967-10de6ba17061?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1153&q=80",
+    snippet: "Look at the colors on your plate. Is all of your food brown or beige? Then it is likely that antioxidant levels are low. Add in foods with rich color like kale, beets, and berries.",
+    date: "November, 15, 2021",
+    content: "Superfoods are foods that have a very high nutritional density. This means that they provide a substantial amount of nutrients and very few calories. Top superfoods: Berries, Soybeans, Green Tea, Leafy Greens, Salmon, Dark Chocolate, Nuts and Grapes",
+  },
+  { 
+    title: "Can Music Improve Your Workouts?",
+    author: "By: Emily Freeman",
+    photo: "https://images.unsplash.com/photo-1586790167861-6e87cdff91b9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+    snippet: "Music — your all-natural pre-workout supplement.",
+    date: "November, 3, 2021",
+    content: "To fully receive the beneficial effects of music on exercise performance, take into consideration what type of activity you’ll be doing. Running for speed? Consider quicker songs with energizing beats. Calming your mind with a zen yoga flow? Press play on meditative music that helps you feel grounded. Mentally preparing for a sweaty HIIT or strength class? Choose songs that give you an emotional boost. And when you want to feel like a total rockstar, you can never go wrong with some Beyonce or Kendrick Lamar (in my opinion).",
+  },
+]
+
+export function getPosts() {
+  return blogList;
+}
